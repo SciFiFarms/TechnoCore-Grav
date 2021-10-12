10c0169d2426a8c520b4d7db73c8f70723012b4e
Spencer Hachmeister <tms@spencerslab.com>
2021-10-11 19:42:19 -0600

    Move grav to linuxserver image and plugin management to the Docker image.

--- migrations/20201107002817-create-admin-user-migrate.sh
+++ migrations/20201107002817-create-admin-user-migrate.sh
@@ -1,7 +1,4 @@
 
 sleep 5
-echo "MADE IT!!!!"
-
-bin/gpm install learn2-git-sync quark page-toc
-bin/plugin login new-user --user "$ADMIN_USER" --password "$(cat /run/secrets/admin_password)" --email "$ADMIN_EMAIL" --fullname "Administrator" -P a 
-echo "Done"
+cd /app/grav-admin/
+/app/grav-admin/bin/plugin login new-user --user "$ADMIN_USER" --password "$(cat /run/secrets/admin_password)" --email "$ADMIN_EMAIL" --fullname "Administrator" -P a
